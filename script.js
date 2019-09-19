6af3edb66bae0d71d5052c786cc2ea506cd14987
Sébastien Chantrie <sebastien.chantrie@gmail.com>
2019-09-19 13:45:33 +0200

    'ajout'

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let first = document.getElementsByClassName('firstsection');
-let second = document.getElementsByClassName('secondsection');
+let first = document.getElementById('section1');
+let firstinfo = document.getElementById('infosection')
+let second = document.getElementById('section2');
 /*
 let three = document.getElementsByClassName('threesection');
 let for = document.getElementsByClassName('forsection');
 let five = document.getElementsByClassName('fivesection');
 */
 
-document.getElementById("btn1").addEventListener("click", function(){
-
+document.getElementById("btn1").addEventListener("click",() => {
+    first.classList.add('animation');
+    firstinfo.classList.add('animation');
 });
