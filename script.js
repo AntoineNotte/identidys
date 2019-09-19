7bd9d83c3c582d418cbe48ec9a5465011833e86e
Sébastien Chantrie <sebastien.chantrie@gmail.com>
2019-09-19 11:27:26 +0200

    'ajoujou'

---
+++ script.js
@@ -0,0 +1,11 @@
+let first = document.getElementsByClassName('firstsection');
+let second = document.getElementsByClassName('secondsection');
+/*
+let three = document.getElementsByClassName('threesection');
+let for = document.getElementsByClassName('forsection');
+let five = document.getElementsByClassName('fivesection');
+*/
+
+document.getElementById("btn1").addEventListener("click", function(){
+
+});
